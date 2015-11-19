c3397907e3c2cc423ef941449f4253228fd91a17
fmdkdd <fmdkdd@gmail.com>
2015-11-19 17:28:18 +0100

    Case study order

--- tex/export-setup.el
+++ tex/export-setup.el
@@ -208,6 +208,163 @@ contextual information."
 
   (advice-add 'org-latex-src-block :around #'fmdkdd//org-latex-src-block)
 
+  ;; For \includegraphics[\linewidth] instead of the default 0.9\linewidth
+  (setq org-latex-image-default-width "\\linewidth")
+
+  ;; Redefine this ox-latex function just to add a `\protect' in front of
+  ;; \includegraphics, so I can include images in figure captions.  A bit
+  ;; heavy-handed, and I'm not entirely sure the `\protect' is benign, but it
+  ;; works.  I also add a `margin' option to the `float' attribute passed by
+  ;; ATTR_LATEX in Org, to trigger the `marginfigure' environment of
+  ;; tufte-latex.
+  (defun fmdkdd//org-latex--inline-image (orig link info)
+    "Return LaTeX code for an inline image.
+LINK is the link pointing to the inline image.  INFO is a plist
+used as a communication channel."
+    (let* ((parent (org-export-get-parent-element link))
+           (path (let ((raw-path (org-element-property :path link)))
+                   (if (not (file-name-absolute-p raw-path)) raw-path
+                     (expand-file-name raw-path))))
+           (filetype (file-name-extension path))
+           (caption (org-latex--caption/label-string parent info))
+           (caption-above-p (org-latex--caption-above-p link info))
+           ;; Retrieve latex attributes from the element around.
+           (attr (org-export-read-attribute :attr_latex parent))
+           (float (let ((float (plist-get attr :float)))
+                    (cond ((string= float "wrap") 'wrap)
+                          ((string= float "sideways") 'sideways)
+                          ((string= float "multicolumn") 'multicolumn)
+                          ((string= float "margin") 'margin)
+                          ((or float
+                               (org-element-property :caption parent)
+                               (org-string-nw-p (plist-get attr :caption)))
+                           (if (and (plist-member attr :float) (not float))
+                               'nonfloat
+                             'figure))
+                          ((and (not float) (plist-member attr :float)) nil))))
+           (placement
+            (let ((place (plist-get attr :placement)))
+              (cond
+               (place (format "%s" place))
+               ((eq float 'wrap) "{l}{0.5\\textwidth}")
+               ((eq float 'figure)
+                (format "[%s]" (plist-get info :latex-default-figure-position)))
+               (t ""))))
+           (comment-include (if (plist-get attr :comment-include) "%" ""))
+           ;; It is possible to specify width and height in the
+           ;; ATTR_LATEX line, and also via default variables.
+           (width (cond ((plist-get attr :width))
+                        ((plist-get attr :height) "")
+                        ((eq float 'wrap) "0.48\\textwidth")
+                        (t (plist-get info :latex-image-default-width))))
+           (height (cond ((plist-get attr :height))
+                         ((or (plist-get attr :width)
+                              (memq float '(figure wrap))) "")
+                         (t (plist-get info :latex-image-default-height))))
+           (options (let ((opt (or (plist-get attr :options)
+                                   (plist-get info :latex-image-default-option))))
+                      (if (not (string-match "\\`\\[\\(.*\\)\\]\\'" opt)) opt
+                        (match-string 1 opt))))
+           image-code)
+      (if (member filetype '("tikz" "pgf"))
+          ;; For tikz images:
+          ;; - use \input to read in image file.
+          ;; - if options are present, wrap in a tikzpicture environment.
+          ;; - if width or height are present, use \resizebox to change
+          ;;   the image size.
+          (progn
+            (setq image-code (format "\\input{%s}" path))
+            (when (org-string-nw-p options)
+              (setq image-code
+                    (format "\\begin{tikzpicture}[%s]\n%s\n\\end{tikzpicture}"
+                            options
+                            image-code)))
+            (when (or (org-string-nw-p width) (org-string-nw-p height))
+              (setq image-code (format "\\resizebox{%s}{%s}{%s}"
+                                       (if (org-string-nw-p width) width "!")
+                                       (if (org-string-nw-p height) height "!")
+                                       image-code))))
+        ;; For other images:
+        ;; - add width and height to options.
+        ;; - include the image with \includegraphics.
+        (when (org-string-nw-p width)
+          (setq options (concat options ",width=" width)))
+        (when (org-string-nw-p height)
+          (setq options (concat options ",height=" height)))
+        (let ((search-option (org-element-property :search-option link)))
+          (when (and search-option
+                     (equal filetype "pdf")
+                     (org-string-match-p "\\`[0-9]+\\'" search-option)
+                     (not (org-string-match-p "page=" options)))
+            (setq options (concat options ",page=" search-option))))
+        (setq image-code
+              (format "\\protect\\includegraphics%s{%s}"
+                      (cond ((not (org-string-nw-p options)) "")
+                            ((= (aref options 0) ?,)
+                             (format "[%s]"(substring options 1)))
+                            (t (format "[%s]" options)))
+                      path))
+        (when (equal filetype "svg")
+          (setq image-code (replace-regexp-in-string "^\\\\includegraphics"
+                                                     "\\includesvg"
+                                                     image-code
+                                                     nil t))
+          (setq image-code (replace-regexp-in-string "\\.svg}"
+                                                     "}"
+                                                     image-code
+                                                     nil t))))
+      ;; Return proper string, depending on FLOAT.
+      (case float
+        (wrap (format "\\begin{wrapfigure}%s
+%s\\centering
+%s%s
+%s\\end{wrapfigure}"
+                      placement
+                      (if caption-above-p caption "")
+                      comment-include image-code
+                      (if caption-above-p "" caption)))
+        (sideways (format "\\begin{sidewaysfigure}
+%s\\centering
+%s%s
+%s\\end{sidewaysfigure}"
+                          (if caption-above-p caption "")
+                          comment-include image-code
+                          (if caption-above-p "" caption)))
+        (multicolumn (format "\\begin{figure*}%s
+%s\\centering
+%s%s
+%s\\end{figure*}"
+                             placement
+                             (if caption-above-p caption "")
+                             comment-include image-code
+                             (if caption-above-p "" caption)))
+        (figure (format "\\begin{figure}%s
+%s\\centering
+%s%s
+%s\\end{figure}"
+                        placement
+                        (if caption-above-p caption "")
+                        comment-include image-code
+                        (if caption-above-p "" caption)))
+        (margin (format "\\begin{marginfigure}%s
+%s\\centering
+%s%s
+%s\\end{marginfigure}"
+                        placement
+                        (if caption-above-p caption "")
+                        comment-include image-code
+                        (if caption-above-p "" caption)))
+        (nonfloat
+         (format "\\begin{center}
+%s%s
+%s\\end{center}"
+                 (if caption-above-p caption "")
+                 image-code
+                 (if caption-above-p "" caption)))
+        (otherwise image-code))))
+
+  (advice-add 'org-latex--inline-image :around #'fmdkdd//org-latex--inline-image)
+
   )
 
 ;; Allow Babel to execute source blocks in batch mode.
