b02cae88772ba9787baafc7f3bc868c5468866dc
fmdkdd <fmdkdd@gmail.com>
2016-10-27 18:39:43 +0200

    Slides lifted from FOAL talk

--- defense/defense.js
+++ defense/defense.js
@@ -275,53 +275,374 @@ les changements de code`),
     sec("Construire un interpréteur par modules"),
 
     slide([
-      h1("Un simple langage arithmétique"),
+      h1("Un langage d'expressions arithmétiques"),
 
       content(pre(code(html_escape(`
-<term> ::= <num> | <term> + <term>
+<term> ::= <num>
+         | <term> + <term>
 <num>  ::= 0 | 1 | 2 | ...
 
 eval : Term -> Integer
+
+show : Term -> String
 `)))),
+
+      div([
+        p("Ingrédients:"),
+        ul([
+          li("Objets dictionnaires"),
+          li("Délégations par prototypes"),
+          li("Fermetures"),
+          li("Portée dynamique")
+        ]),
+      ], {style: `position: absolute;
+                  top: 125px;
+                  right: 50px;`}),
+
+      img({src: 'img/foal-language.svg',
+           style: `position: absolute;
+                   width: 300px;
+                   left: 80px;
+                   bottom: 50px;`}),
 
     ]),
 
-    // TODO: reuse slides from FOAL
+    // TODO: minimap of terms in corner
 
     slide([
-      h1(`Utiliser ${code('with')}`),
+      h1(`Le terme ${code('num')}`),
+
+      content(pre(code(`var num = {
+  new(n) { return {__proto__: this, n } },
+  eval() { return this.n }}
+
+var e1 = num.new(3)
+e1.eval() //: 3`))),
+
+      p(img({src: 'img/foal-1.svg'}))
+    ]),
+
+    slide([
+      h1(`Ajouter le terme ${code('plus')}`),
+
+      content(pre(code(`var plus = {
+  new(l, r) { return {__proto__: this, l, r } },
+  eval() { return this.l.eval() + this.r.eval() }}
+
+var e2 = plus.new(num.new(1), num.new(2))
+e2.eval() //: 3
+e1.eval() //: 3`))),
+
+      p(img({src: 'img/foal-2.svg'}))
+
+    ]),
+
+    slide([
+      h1("Ajouter une opération, rétroactivement"),
+
+      content(pre(code(`num.show = () => this.n.toString()
+plus.show = () => this.l.show() + '+' + this.r.show()
+
+plus.new(num.new(1), num.new(2)).show() //: "1+2"
+e1.show() //: "3"
+e2.show() //: "1+2"`))),
+
+      p(img({src: 'img/foal-3.svg'}))
+
+    ]),
+
+    slide([
+      h1("Ajouter une opération comme module"),
+
+      content(pre(code(`var show = base => {
+  var num = {__proto__: base.num,
+    show() { return this.n.toString() }}
+  var plus = {...}
+  return {num, plus}}
+
+var s = show({num, plus})
+e2.show() //: undefined
+
+s.plus.new(s.num.new(1), s.num.new(2)).show() //: "1+2"
+s.plus.new(s.num.new(1), s.num.new(2)).eval() //: "1+2"`))),
+
+      p(img({src: 'img/foal-4.svg'}))
+    ]),
+
+    slide([
+      h1("Problème: mixer les modules"),
+
+      content(pre(code(`s.plus.new(num.new(1), s.num.new(2)).show()
+
+//: TypeError: this.l.show is not a function
+
+plus.new: Term -> Term -> Term
+s.plus.new: Show -> Show -> Show
+`))),
+
+    ]),
+
+    slide([
+      h1(`${code('with')} à la rescousse`),
+
+      content(pre(code(`with(show({num, plus})) {
+plus.new(num.new(1), num.new(2)).show() }`))),
 
       p(img({src: 'img/foal-5.svg'})),
 
     ]),
 
+    slide([
+      h1("Modifier l'interpréteur"),
+
+      p(img({src: 'img/foal-ext-language.svg'})),
+    ]),
+
+    slide([
+      h1("Modifier des opérations"),
+
+      content(pre(code(`var double = base => {
+  var num = {__proto__: base.num,
+    eval() { return base.num.eval.call(this) * 2 }}
+
+  return {__proto__: base, num}}
+
+with (double({num})) {
+  plus.new(num.new(1), num.new(2)).eval() //: 6
+}
+plus.new(num.new(1), num.new(2)).eval() //: 3`))),
+
+      p(img({src: 'img/foal-6.svg'})),
+
+    ]),
+
+    slide([
+      h1("Combiner les modifications"),
+
+      content(pre(code(`with(double({num})) {
+  with (double({num})) {
+    with (double({num})) {
+      plus.new(num.new(1), num.new(2)).eval() //: 24`))),
+
+    ]),
+
+    slide([
+      h1("Ajouter de l'état"),
+
+      content(pre(code(`var state = (base, count = 0) => {
+var num = {__proto__: base.num,
+           eval() {
+             count++;
+             return base.num.eval.call(this) }},
+
+var plus = {...}
+var getCount = () => count
+
+return {__proto__: base, num, plus, getCount}}
+
+with (state({num, plus})) {
+  getCount() //: 0
+  plus.new(num.new(1), num.new(2)).eval() //: 3
+  getCount() //: 3
+}`))),
+    ]),
+
+    slide([
+      h1("All together now"),
+
+      content(pre(code(`with (state({num, plus})) {
+  with (double({num})) {
+    with (show({num, plus})) {
+      getCount() //: 0
+      var n = plus.new(num.new(1), num.new(2))
+      n.eval() //: 6
+      getCount() //: 3
+      n.show() //: "1+2"
+}}}`))),
+    ]),
+
+    slide([
+      h1("Un interpréteur modulaire"),
+
+      p("Ingrédients:"),
+      ul([
+        li("Objets dictionnaires"),
+        li("Délégations par prototypes"),
+        li("Fermetures"),
+        li(`Portée dynamique (${code('with')})`)
+      ]),
+
+      p(img({src: 'img/foal-full-language.svg',
+             style: `width: 350px;
+                     position: absolute;
+                     top: 150px;
+                     right: 80px;`})),
+    ]),
 
     sec("Étendre Narcissus par manipulation de portée"),
 
     slide([
-      h1("Le motif module"),
+      h1("Le motif module dans Narcissus"),
 
-      p("Complexité supplémentaire de Narcissus"),
-      p("Verrouille l'extension")
+      p("Verrouille l'extension"),
+
+      content(pre(code(`var Narcissus = (function(){
+  var globalBase = { ... }
+
+  function ExecutionContext(type, version) { ... }
+  function getValue(v) { ... }
+  function putValue(v, w) { ... }
+  function evaluate(code) { ... }
+
+  return {
+    globalBase: globalBase,
+    evaluate: evaluate,
+    ...
+  }
+}())`))),
+    ]),
+
+    slide([
+      h1("La portée dans le motif module"),
+
+      content(pre(code(`var m = (function(){
+  var a = 1
+  function f(x) { return x + a }
+  function g(x) { return f(x) }
+  return {g: g}
+}())
+
+m.g(0) //: 1`))),
+
+      p(img({src: 'img/dls0.svg'}))
     ]),
 
     slide([
       h1("Ouvrir le motif module"),
 
-      p(img({src: 'img/dls12.svg'}))
+      content(pre(code(`var m = (function(){
+  var a = 1
+  function f(x) {
+    return x + a }
+  function g(x) {
+    return f(x) }
+  return {g: g}
+}())
+
+m.g(0) //: 1
+m.E.a = 2
+m.g(0) //: 2`))),
+
+      p(img({src: 'img/dls4.svg',
+             style: `position: absolute;
+                     width: 350px;
+                     top: 180px;
+                     right: 80px;`}))
     ]),
+
+    slide([
+      h1("Ajouter un environnement frontal"),
+
+      content(pre(code(`var m = (function(){ ... }())
+
+m.g(0) //: 1
+m.E.a = 2
+m.g(0) //: 2
+delete m.E.a
+m.g(0) //: 1`))),
+
+      p(img({src: 'img/dls7.svg'}))
+    ]),
+
+    slide([
+      h1(`Ouvrir le motif module avec ${code('with')}`),
+
+      content(pre(code(`var m = (function(){
+  var E = Object.create(null)
+  with (E) {
+    var a = 1
+    function f(x) {
+      return x + a }
+    function g(x) {
+      return f(x) }
+    return {
+      g: g,
+      E: E }
+  }
+}())
+
+m.g(0) //: 1
+m.E.a = 2
+m.g(0) //: 2`))),
+
+      p(img({src: 'img/dls11.svg',
+             style: `position: absolute;
+                     width: 350px;
+                     top: 230px;
+                     right: 80px;`}))
+    ]),
+
+    slide([
+      h1("Combiner les extensions"),
+
+      content(pre(code(`var m = (function(){ ... }())
+
+m.g(0) //: 1
+
+var e1 = { a: 2, f(x) {
+  return x + 2 * m.E.a }}
+pushEnv(e1, m.E)
+m.g(0) //: 4
+
+var e2 = { f(x) {
+  return -m.E.a }}
+pushEnv(e2, m.E)
+m.g(0) //: -2
+
+removeEnv(e1, m.E)
+m.g(0) //: -1`))),
+
+      p(img({src: 'img/dls8.svg',
+             style: `position: absolute;
+                     width: 300px;
+                     top: 180px;
+                     right: 80px;`}))
+
+    ]),
+
+    slide([
+      h1("Appliquer sur Narcissus"),
+
+      content(pre(code(`Narcissus.interpreter = (function(){
++  var _env = Object.create(null)
++  with (_env) {
+
+   ... /* 1500 lignes de code */ ...
+
++  var _parent = { ... }
++  Object.setPrototypeOf(_env, _parent);
+   return {
+     evaluate: evaluate,
+     ...
++    _env: _env
+   };
++  }
+}())`)))
+    ]),
+
+    slide(figure(img({src: 'img/narcissus-diff-after.svg'}))),
 
     slide([
       h1("Évaluation"),
 
       ul([
         li("4 analyses ajoutées"),
-        li("XX lignes modifiées"),
-        li("spécifique à ce cas précis")
+        li("51 lignes de Narcissus modifiées"),
+        li("résultats identiques sur test262"),
+        li("7% d'overhead sur l'analyse multi-facettes")
       ])
     ]),
 
-    slide(figure(img({src: 'img/narcissus-diff-after.svg'}))),
 
     sec("Conclusions"),
 
